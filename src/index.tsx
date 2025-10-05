a72e33c03e5af4caac9a5c8e215b5b87f4388450
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2025-10-05 14:50:00 +0530

    reactQuery debug option

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
+
+const client = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
+  <QueryClientProvider client={client}>
   <BrowserRouter>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
   </BrowserRouter>
+</QueryClientProvider>
 );
